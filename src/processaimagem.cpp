68b153b5e4f436ef79055fcd3d2d22d1ffb22997
heitorc62 <heitor.c@usp.br>
2023-02-28 09:12:25 -0300

    Corrigimos erro na função hue. Processo de correção da avaliação de qual pixel mais se assemelha ao central. Essa funcionalidade não está funcionando ainda.

--- src/processaimagem.cpp
+++ src/processaimagem.cpp
@@ -7,7 +7,7 @@ uint32_t vetoriza(uint32_t x, uint32_t y, int largura) {
 }
 
 int* vals_rgb(uint32_t ponto, uint8_t *rgb) {
-    int vals[3];
+    static int vals[3];
     vals[0] = rgb[3*ponto];
     vals[1] = rgb[(3*ponto) + 1];
     vals[2] = rgb[(3*ponto) + 2];
@@ -15,13 +15,11 @@ int* vals_rgb(uint32_t ponto, uint8_t *rgb) {
 }
 
 int hue(double r, double g, double b){
-
     r = r / 255.0; g = g / 255.0; b = b / 255.0;
 
     double cmax = max(r, max(g, b)); // maximum of r, g, b
     double cmin = min(r, min(g, b)); // minimum of r, g, b
     double diff = cmax - cmin; // diff of cmax and cmin.
-
     double h = -1;
 
     if (cmax == cmin)
@@ -35,7 +33,6 @@ int hue(double r, double g, double b){
 
     else if (cmax == b)
         h = fmod(60 * ((r - g) / diff) + 240, 360);
-
     return h;
 }
 
@@ -58,12 +55,32 @@ String mais_proximo_util_2(int i){
 String mais_proximo_util(double valor, double* lista) {
     double min = INT_MAX;
     int index = 0;
+
+    Serial.print("Valor hue do centro = ");
+    Serial.println(valor);
+
+    Serial.print("Valor do hue da esquerda (0) = ");
+    Serial.println(lista[0]);
+
+    Serial.print("Valor do hue da direita (1) = ");
+    Serial.println(lista[1]);
+
+    Serial.print("Valor do hue superior (2) = ");
+    Serial.println(lista[2]);
+
+    Serial.print("Valor do hue inferior (3) = ");
+    Serial.println(lista[3]);
+
+
+
     for(int i = 0; i < 4; i++){
         if(abs(int(valor) - int(lista[i])) < min){
             min = int(valor) - int(lista[i]);
             index = i;
         }
     }
+    Serial.print("Index mais próximo: ");
+    Serial.println(index);
 
     return mais_proximo_util_2(index);
 }
@@ -73,18 +90,28 @@ String mais_proximo_util(double valor, double* lista) {
 String processa_imagem(uint8_t *rgb, int altura, int largura) {
     uint32_t ponto_esquerdo_x = (uint32_t)(1 / 4. * largura);
     uint32_t ponto_esquerdo_y = (uint32_t)(1 / 2. * altura);
+    Serial.print("Coordenadas ponto esquerdo: ");
+    Serial.println(ponto_esquerdo_x, ponto_esquerdo_y);
 
-    uint32_t ponto_direito_x = (uint32_t)(1 / 4. * largura);
+    uint32_t ponto_direito_x = (uint32_t)(3 / 4. * largura);
     uint32_t ponto_direito_y = (uint32_t)(1 / 2. * altura);
+    Serial.print("Coordenadas ponto direito: ");
+    Serial.println(ponto_direito_x, ponto_direito_y);
 
-    uint32_t ponto_superior_x = (uint32_t)(1 / 4. * largura);
-    uint32_t ponto_superior_y = (uint32_t)(1 / 2. * altura);
+    uint32_t ponto_superior_x = (uint32_t)(1 / 2. * largura);
+    uint32_t ponto_superior_y = (uint32_t)(1 / 4. * altura);
+    Serial.print("Coordenadas ponto superior: ");
+    Serial.println(ponto_superior_x, ponto_superior_y);
 
-    uint32_t ponto_inferior_x = (uint32_t)(1 / 4. * largura);
-    uint32_t ponto_inferior_y = (uint32_t)(1 / 2. * altura);
+    uint32_t ponto_inferior_x = (uint32_t)(1 / 2. * largura);
+    uint32_t ponto_inferior_y = (uint32_t)(3 / 4. * altura);
+    Serial.print("Coordenadas ponto inferior: ");
+    Serial.println(ponto_inferior_x, ponto_inferior_y);
 
-    uint32_t ponto_central_x = (uint32_t)(1 / 4. * largura);
+    uint32_t ponto_central_x = (uint32_t)(1 / 2. * largura);
     uint32_t ponto_central_y = (uint32_t)(1 / 2. * altura);
+    Serial.print("Coordenadas ponto central: ");
+    Serial.println(ponto_central_x, ponto_central_y);
 
     uint32_t ponto_esquerdo_vetorizado = vetoriza(ponto_esquerdo_x, ponto_esquerdo_y, largura);
     uint32_t ponto_direito_vetorizado = vetoriza(ponto_direito_x, ponto_direito_y, largura);
@@ -92,18 +119,17 @@ String processa_imagem(uint8_t *rgb, int altura, int largura) {
     uint32_t ponto_inferior_vetorizado = vetoriza(ponto_inferior_x, ponto_inferior_y, largura);
     uint32_t ponto_central_vetorizado = vetoriza(ponto_central_x, ponto_central_y, largura);
 
-    int *esquerdo = vals_rgb(ponto_esquerdo_vetorizado, rgb);
-    int *direito = vals_rgb(ponto_esquerdo_vetorizado, rgb);
-    int *superior = vals_rgb(ponto_esquerdo_vetorizado, rgb);
-    int *inferior = vals_rgb(ponto_esquerdo_vetorizado, rgb);
-    int *centro = vals_rgb(ponto_central_vetorizado, rgb);
+    int* esquerdo = vals_rgb(ponto_esquerdo_vetorizado, rgb);
+    int* direito = vals_rgb(ponto_esquerdo_vetorizado, rgb);
+    int* superior = vals_rgb(ponto_esquerdo_vetorizado, rgb);
+    int* inferior = vals_rgb(ponto_esquerdo_vetorizado, rgb);
+    int* centro = vals_rgb(ponto_central_vetorizado, rgb);
 
     double hues[4];
     hues[0] = hue(esquerdo[0], esquerdo[1], esquerdo[2]);
     hues[1] = hue(direito[0], direito[1], direito[2]);
     hues[2] = hue(superior[0], superior[1], superior[2]);
     hues[3] = hue(inferior[0], inferior[1], inferior[2]);
-    
     double hue_centro = hue(centro[0], centro[1], centro[2]);
 
     
